3fb6865dab4d301b6e047891ab36b3dba1cb79bd
Humna Mir <22HMir@heckgrammar.co.uk>
2025-04-04 10:10:03 +0100

    :)

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            DateTime current = DateTime.Now;
+            DateTime target = new DateTime(2025, 4, 4, 15, 15, 0);
+            TimeSpan difference = target - current;
+            Console.WriteLine(difference.ToString());
+            while (true)
+            {
+                Console.WriteLine(difference.ToString());
+                Thread.Sleep(1000);
+            }
         }
     }
 }
